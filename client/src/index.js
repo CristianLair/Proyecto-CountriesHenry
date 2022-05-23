90aef46ca5125ee92a4f7d6f7855d39e4408f2c9
crisitan lair <crisitanlair@gmail.com>
2022-05-23 13:53:21 -0300

    client

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
